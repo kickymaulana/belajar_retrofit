6f4cc317dc1b5b7f5cc6bcbda78be936573c35f1
kickymaulana <kickymaulana@gmail.com>
2025-01-28 20:40:10 +0700

    menambahkan loading

--- app/src/main/java/com/kickymaulana/belajarroomdatabase/userlist/PegawaiListScreen.kt
+++ app/src/main/java/com/kickymaulana/belajarroomdatabase/userlist/PegawaiListScreen.kt
@@ -1,5 +1,7 @@
 package com.kickymaulana.belajarroomdatabase.userlist
 
+import android.graphics.Paint.Align
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,15 +11,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemContentType
 import org.koin.androidx.compose.koinViewModel
@@ -55,6 +60,15 @@ fun PegawaiListScreen(navController: NavController, viewModel: PegawaiListViewMo
                 .padding(scaffoldPadding)
         ) {
 
+            if(lazyPagingItems.loadState.prepend is LoadState.Loading){
+                item{
+                    Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
+                        CircularProgressIndicator()
+
+                    }
+                }
+            }
+
             items(
                 count = lazyPagingItems.itemCount,
                 contentType = lazyPagingItems.itemContentType { "contentType" }
@@ -68,6 +82,15 @@ fun PegawaiListScreen(navController: NavController, viewModel: PegawaiListViewMo
                     }
                 }
                 Spacer(Modifier.height(6.dp))
+            }
+
+            if(lazyPagingItems.loadState.append is LoadState.Loading){
+                item{
+                    Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
+                        CircularProgressIndicator()
+
+                    }
+                }
             }
 //            items(10) {
 //                Card(modifier = Modifier.fillParentMaxWidth()) {
